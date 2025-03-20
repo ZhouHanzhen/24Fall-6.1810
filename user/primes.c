76ba478678b06a1652ff81035d59e8eec7ff840b
Hanzhen <zhouhanzhen2023@gmail.com>
2025-03-20 21:44:44 +0800

    add some comments for primes

--- user/primes.c
+++ user/primes.c
@@ -12,16 +12,16 @@ int main(int argc, char *argv[]){
     pipe(p);
 
     if(fork() == 0) {
-        close(p[1]);
-        primes(p[0]);
-        exit(0);
+        close(p[1]); // close write end p[1] for child process
+        primes(p[0]); // child process read from p[0] and write to p_next[1] and recursively call primes
+        exit(0); // child process exit
     }else {
-        close(p[0]);
+        close(p[0]); // close read end p[0] for parent process
         for(i = 2; i <= 280; i++) {
-            write(p[1], &i, 4);
+            write(p[1], &i, 4); // the first parent process write 2-280 to p[1] 
         } 
-        close(p[1]);
-        wait(0);
+        close(p[1]); // parent process' write to p[1] ends; 
+        wait(0); // wait for the child process to end
     }
 
     return 0;
@@ -47,9 +47,9 @@ void primes(int p_0) {
             printf("prime %d\n", i);
             
             close(p_next[0]);
-            while(read(p_0, &n, 4)){
-                if( n % i ) {
-                    write(p_next[1], &n, 4);
+            while(read(p_0, &n, 4)){      // read from p[0]
+                if( n % i ) {               // 筛掉倍数
+                    write(p_next[1], &n, 4);  // write to p_next[1]
                 }
             }
             close(p_0); // read from p[0] ends;
